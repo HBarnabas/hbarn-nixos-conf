ddd2f55b70ac1a4ad3ecc5219e8fb97cf2e12cf9
HBarnabas <hbarnabas@outlook.com>
2026-02-03 14:25:18 +0100

    add alt stable input

--- flake.nix
+++ flake.nix
@@ -2,16 +2,23 @@
   description = "flake for nixos";
 
   inputs = {
+    # default (unstable)
     nixpkgs = {
       url = "github:NixOS/nixpkgs/nixos-unstable";
     };
+
+    # optional alternative (stable)
+    nixpkgs-stable = {
+      url = "github:NixOS/nixpkgs?ref=release-25.11";
+    };
+
     home-manager = {
       url = "github:nix-community/home-manager";
       inputs.nixpkgs.follows = "nixpkgs";
     };
   };
 
-  outputs = { self, nixpkgs, home-manager }:
+  outputs = { self, nixpkgs, nixpkgs-stable, home-manager }:
     let
       overlays = [
         (import ./overlays/ps4-pkg-tools.nix)
@@ -33,6 +40,20 @@
             {
               home-manager.useGlobalPkgs = true;
               home-manager.useUserPackages = true;
+
+              # Expose stable as `stable` for Home Manager modules.
+              #
+              # Usage inside HM modules:
+              #   { pkgs, stable, ... }:
+              #   { home.packages = [ stable.zed-editor ]; }
+              home-manager.extraSpecialArgs = {
+                stable = import nixpkgs-stable {
+                  system = "x86_64-linux";
+                  overlays = overlays;
+                  config.allowUnfree = true;
+                };
+              };
+
               home-manager.users.hbarn = {
                 imports = [
                   ./home/hbarn/home.nix
