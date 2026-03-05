46d5510dfba0e21eb17220a7a174e3e08ac650f9
HBarnabas <hbarnabas@outlook.com>
2026-03-05 16:17:28 +0100

    fix virt-secret-init

---
+++ hosts/desktop/modules/virt-secret-fix.nix
@@ -0,0 +1,17 @@
+{ config, pkgs, ... }:
+
+{
+  # Fix for virt-secret-init-encryption.service using /usr/bin/sh
+  # NixOS doesn't use FHS paths like /usr/bin, so we need to override this service
+
+  # Option 1: Override the service to use proper NixOS paths (use if you need encrypted VM secrets)
+  systemd.services.virt-secret-init-encryption = {
+    serviceConfig = {
+      ExecStart = "${pkgs.bash}/bin/bash";
+    };
+    path = [ pkgs.bash pkgs.coreutils ];
+  };
+
+  # Option 2: Disable the service entirely (uncomment if you don't use encrypted VM secrets)
+  # systemd.services.virt-secret-init-encryption.enable = false;
+}
